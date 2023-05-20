af10cc7a17bd35187b09bf7d9e7bc6e7d7a7a5c5
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-05-21 03:55:58 +0600

    created my toys UI perfect

--- src/pages/MyToy/MyToy.jsx
+++ src/pages/MyToy/MyToy.jsx
@@ -4,74 +4,90 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProviders";
 
 const MyToy = () => {
-    const { user } = useContext(AuthContext);
-    console.log(user);
-    const [myToys, setMyToys] = useState([]);
-    console.log(myToys)
+  const { user } = useContext(AuthContext);
+  console.log(user);
+  const [myToys, setMyToys] = useState([]);
+  console.log(myToys);
 
-    const handleDelete = id => {
-        const proceed = confirm('Are you sure to delete');
-        if (proceed) {
-            fetch(`https://toys-and-hobbies-server.vercel.app/toy/${id}`, {
-                method: "DELETE"
-            })
-                .then(res => res.json())
-                .then(data => {
-                    console.log(data);
-                    if (data.deletedCount > 0) {
-                        alert('Deleted successfully');
-                        const remaining = myToys.filter(toys => toys._id !== id);
-                        setMyToys(remaining);
-                    }
-                })
-        }
+  const handleDelete = (id) => {
+    const proceed = confirm("Are you sure to delete");
+    if (proceed) {
+      fetch(`https://toys-and-hobbies-server.vercel.app/toy/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            alert("Deleted successfully");
+            const remaining = myToys.filter((toys) => toys._id !== id);
+            setMyToys(remaining);
+          }
+        });
     }
+  };
 
-    const url = `https://toys-and-hobbies-server.vercel.app/my-toys?email=${user?.email}`;
+  const url = `https://toys-and-hobbies-server.vercel.app/my-toys?email=${user?.email}`;
 
-    useEffect(() => {
-        fetch(url)
-            .then(res => res.json())
-            .then(data => setMyToys(data))
-    }, [url])
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setMyToys(data));
+  }, [url]);
 
-    return (
-        <div className="w-11/12 mx-auto">
-            <h2 className="font-bold text-4xl text-center my-12 md:my-16 underline">My Toys</h2>
-            <div className="overflow-x-auto ">
-                <table className="table w-full">
-                    {/* head */}
-                    <thead>
-                        <tr className="">
-                            <th>S/N</th>
-                            <th>Toy Name</th>
-                            <th>Toy Category</th>
-                            <th>Toy Price</th>
-                            <th>Toy Quantity</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            myToys.map((toy, index) =>
-                                <tr key={toy._id} className="">
-                                    <th>{index + 1}</th>
-                                    <td>{toy.name}</td>
-                                    <td>{toy.subcategory}</td>
-                                    <td>$ {toy.price}</td>
-                                    <td>{toy.quantity}</td>
-                                    <td className="space-x-1">
-                                        <Link to={`/update-toy/${toy._id}`} className="btn btn-warning">Update</Link>
-                                        <button onClick={() => handleDelete(toy._id)} className="btn btn-danger">Delete</button>
-                                    </td>
-                                </tr>
-                            )
-                        }
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="w-11/12 mx-auto">
+      <h2 className="font-bold text-4xl text-center my-12 md:my-16 underline">
+        My Toys
+      </h2>
+      <div className="overflow-x-auto mb-8">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr className="">
+              <th></th>
+              <th>Toy</th>
+              <th>Toy Name</th>
+              <th>Seller</th>
+              <th>Sub Category</th>
+              <th>Price</th>
+              <th>Available Quantity</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {myToys.map((toy, index) => (
+              <tr key={toy._id} className="">
+                <th>{index + 1}</th>
+                <th>
+                    <img className="w-16" src={toy.picture} alt="" />
+                </th>
+                <td>{toy.name}</td>
+                <td>{toy.SellerEmail}</td>
+                <td>$ {toy.subcategory}</td>
+                <td>{toy.price}</td>
+                <td>{toy.quantity}</td>
+                <td className="space-x-1">
+                  <Link
+                    to={`/update-toy/${toy._id}`}
+                    className="btn  bg-rose-600 hover:bg-rose-600"
+                  >
+                    Update
+                  </Link>
+                  <button
+                    onClick={() => handleDelete(toy._id)}
+                    className="btn btn-danger"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 
 export default MyToy;
